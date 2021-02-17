ff016f8fbfa6c2d9febe34a9c290169d619e82f3
Homero Esmeraldo <homero.esmeraldo@gmail.com>
2021-02-16 22:42:16 -0300

    Update mMaze to have choice point

--- runSimulation_mMaze.m
+++ runSimulation_mMaze.m
@@ -1,27 +1,15 @@
 %% STATE-SPACE PARAMETERS
 setParams;
 % single m maze with 2 goals
-% params.maze             = zeros(6,7); % zeros correspond to 'visitable' states
-% params.maze(2:6,2:3)      = 1; % wall
-% params.maze(2:6,5:6)      = 1; % wall
-% params.s_start          = [6,4]; % beginning state (in matrix notation)
-% params.s_start_rand     = false; % Start at random locations after reaching goal
-% params.s_end            = [6,1;6,7]; % goal state (in matrix notation)
-% params.rewMag           = [1; 1]; % reward magnitude (rows: locations; columns: values)
-
-
-% two m mazes with 2 starting points and 2 goals
-params.maze             = zeros(6,15); % zeros correspond to 'visitable' states
+params.maze             = zeros(6,7); % zeros correspond to 'visitable' states
 params.maze(2:6,2:3)      = 1; % wall
 params.maze(2:6,5:6)      = 1; % wall
-params.maze(1:6,8)          = 1; % wall
-params.maze(2:6,10:11)      = 1; % wall
-params.maze(2:6,13:14)      = 1; % wall
-params.s_start          = [6,4; 6,12]; % beginning state (in matrix notation)
+params.s_start          = [6,4]; % beginning state (in matrix notation)
 params.s_start_rand     = false; % Start at random locations after reaching goal
-params.s_end            = [6,1;6,15]; % [6,1;6,7;6,9;6,15]; goal state (in matrix notation)
-params.rewMag           = [1,0;0,1]; % reward magnitude (rows: locations; columns: values)
+params.s_end            = [6,1;6,7]; % goal state (in matrix notation)
+params.rewMag           = [1; 1]; % reward magnitude (rows: locations; columns: values)
 
+params.s_choice = [2,4];
 
 % linear track 2 starting locations, 2 goals
 % params.maze             = zeros(3,10); % zeros correspond to 'visitable' states
@@ -38,14 +26,13 @@ params.rewMag           = [1,0;0,1]; % reward magnitude (rows: locations; column
 params.rewSTD           = 0.1; % reward Gaussian noise (rows: locations; columns: values)
 params.rewProb          = 1; % probability of receiving each reward (columns: values)
 
-
 %% PLOTTING SETTINGS
 params.PLOT_STEPS       = true; % Plot each step of real experience
 params.PLOT_Qvals       = true; % Plot Q-values
 params.PLOT_PLANS       = true; % Plot each planning step
 params.PLOT_EVM         = true; % Plot need and gain
 params.PLOT_TRACE       = false; % Plot all planning traces
-params.PLOT_wait        = 1 ; % Number of full episodes completed before plotting
+params.PLOT_wait        = 5 ; % Number of full episodes completed before plotting
 
 
 %% RUN SIMULATION
